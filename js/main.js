72283d6f96a5901f07e9c75832cffb87703500a8
António Fernandes <antonio.mf97@gmail.com>
2023-11-01 15:14:29 +0000

    Integrated logic to frontend

--- js/main.js
+++ js/main.js
@@ -2,49 +2,60 @@ const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib;
 
 let pdfDoc;
 
+
 async function loadPdf() {    
-	// Fetch the template PDF document
-	const url = './template/CMI_DECASA_2023.pdf';
-	
-    // Convert to byte buffer
-    const templatePdfBytes = await fetch(url).then((res) =>
-		res.arrayBuffer(),
-	);
+    // Fetch the template PDF document
+    const url = './template/CMI_DECASA_2023.pdf';
+        // Convert to byte buffer
+        const templatePdfBytes = await fetch(url).then((res) => res.arrayBuffer(),
+    );
 
 	// Load a PDFDocument object as 
 	pdfDoc = await PDFDocument.load(templatePdfBytes);
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
     
+	// get pages from PDF
     const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    console.log(firstPage);
-    var { width, height } = firstPage.getSize();
-
-    for(let i=0; i < 62; i++){
-      firstPage.drawText(document.forms["form"][i].value, {
-          x: width * positions[i][0],
-          y: height * positions[i][1],
-          size: 7,
-          font: helveticaFont,
-          color: rgb(1, 0, 0)
-      })
-    }
-
-    const secondPage = pages[1];
-    console.log(secondPage);
-    var { width, height } = secondPage.getSize();
     
-    for(let i=62; i < 72; i++){
-      secondPage.drawText(document.forms["form"][i].value, {
-          x: width * positions[i][0],
-          y: height * positions[i][1],
-          size: 7,
-          font: helveticaFont,
-          color: rgb(1, 0, 0)
-      })
-    }
+	var label, field;
+	var fields = getFieldsInfo();
 
-    const pdfBytes = await pdfDoc.save();
-    
-    download(pdfBytes, "foo.pdf", "application/pdf");
+	for(var i in labels){
+		label = labels[i];
+		field = fields[label];
+
+		// get right page of placeholder
+		var currentPage = pages[field["page"]];
+		var { width, height } = currentPage.getSize();
+
+		// draw text on the correct field
+		currentPage.drawText(field["value"], {
+			x: width * field["width"],
+			y: height * field["height"],
+			size: 7,
+			font: helveticaFont,
+			color: rgb(1, 0, 0)
+		})
+	}
+
+  	const pdfBytes = await pdfDoc.save();
+  
+	download(pdfBytes, "declaracao.pdf", "application/pdf");
+}
+
+function getFieldsInfo() {
+	var label;
+	var fields = {};
+
+	for(var i in labels) {
+		label = labels[i];
+		fields[label] = {
+			"value" : document.getElementById(label).value,
+			"page" : positions[label][0],
+			"width" : positions[label][1],
+			"height": positions[label][2],
+		}
+	}
+
+	return fields;
 }
